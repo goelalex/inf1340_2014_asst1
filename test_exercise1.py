f711b1a8afc318aae54e491ad2f542c0f51d5cc6
ZhaoH <Zhao.hu@outlook.com>
2014-10-02 10:07:23 -0400

    update grade_to_gpa() function, still have some problem on or statement

--- test_exercise1.py
+++ test_exercise1.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
-""" Module to test exercise1.py """
+""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion
+
+This module contains one function grade_to_gpa. It can be passed a parameter
+that is an integer (0-100) or a letter grade (A+, A, A-, B+, B, B-, or FZ). All
+other inputs will result in an error.
+
+Example:
+    $ python exercise1.py
+
+"""
 
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
@@ -11,69 +20,86 @@ __license__ = "MIT License"
 __status__ = "Prototype"
 
 # imports one per line
-import pytest
-from exercise1 import grade_to_gpa
 
 
-def test_letter_grade():
+def grade_to_gpa(grade):
     """
-    Letter grade inputs
+    Returns the UofT Graduate GPA for a given grade.
+
+    :param:
+        grade (integer or string): Grade to be converted
+            If integer, accepted values are 0-100.
+            If string, accepted values are A+, A, A-, B+, B, B-, FZ
+
+    :return:
+        float: The equivalent GPA
+            Value is 0.0-4.0
+
+    :raises:
+        TypeError if parameter is not a string or integer
+        ValueError if parameter is out of range
     """
-    assert grade_to_gpa("A+") == 4.0
-    assert grade_to_gpa("A") == 4.0
-    assert grade_to_gpa("A-") == 4.0
-    assert grade_to_gpa("B+") == 4.0
-    assert grade_to_gpa("B") == 4.0
-    assert grade_to_gpa("B-") == 4.0
-    assert grade_to_gpa("FZ") == 4.0
-    
-    with pytest.raises(ValueError):
-        grade_to_gpa("q")
-    # add more tests for invalid values
 
+    letter_grade = ""
+    list_grade = ["A+", "A", "A-","B+", "B", "B-","FZ"]
+    gpa = 0.0
 
-def test_percentage_grade():
-    """
-    Numeric grade inputs
-    """
-    assert grade_to_gpa(100) == 4.0
-    assert grade_to_gpa(95) == 4.0
-    assert grade_to_gpa(90) == 4.0
-    
-    assert grade_to_gpa(89) == 4.0
-    assert grade_to_gpa(87) == 4.0
-    assert grade_to_gpa(85) == 4.0
+    if type(grade) is str:
+        print ("letter") # remove this line once the code is implemented
+        # check that the grade is one of the accepted values
+        # assign grade to letter_grade
+        for count in range(0,len(list_grade)):
+            if grade == list_grade[count]:
+                letter_grade = grade
 
-    assert grade_to_gpa(84) == 3.7
-    assert grade_to_gpa(82) == 3.7
-    assert grade_to_gpa(80) == 3.7
+        if letter_grade == "":
+            print("error")
+            raise ValueError("Invalid value passed as parameter")
 
-    assert grade_to_gpa(79) == 3.3
-    assert grade_to_gpa(78) == 3.3
-    assert grade_to_gpa(77) == 3.3
+    elif type(grade) is int:
+        print("mark") # remove this line once the code is implemented
+        # check that grade is in the accepted range
+        # convert the numeric grade to a letter grade
+        # assign the value to letter_grade
+        # hint: letter_grade = mark_to_letter(grade)
+        #if grade > 100 | grade < 0:  #??????? CANNOT USE OR STATEMENT????
+         if
+            print("error")
+            raise ValueError("Invalid type passed as parameter")
+        elif grade >= 90:
+            letter_grade = "A+"
+        elif grade >= 85:
+            letter_grade = "A"
+        elif grade >= 80:
+            letter_grade = "A-"
+        elif grade >= 77:
+            letter_grade = "B+"
+        elif grade >= 73:
+            letter_grade = "B"
+        elif grade >= 70:
+            letter_grade = "B-"
+        else:
+            letter_grade = "FZ"
+    else:
+        # raise a TypeError exception
+        raise TypeError("Invalid type passed as parameter")
 
-    assert grade_to_gpa(76) == 3.0 
-    assert grade_to_gpa(74) == 3.0
-    assert grade_to_gpa(73) == 3.0
+    # write a long if-statement to convert letter_grade
+    # assign the value to gpa
+    if letter_grade == "A+":
+        gpa = 4.0
+    elif letter_grade == "A":
+        gpa = 4.0
+    elif letter_grade == 'A-':
+        gpa = 3.7
+    elif letter_grade == 'B+':
+        gpa = 3.3
+    elif letter_grade == 'B':
+        gpa = 3.0
+    elif letter_grade == 'B-':
+        gpa = 2.7
+    elif letter_grade == 'FZ':
+        gpa = 0.0
 
-    assert grade_to_gpa(72) == 2.7
-    assert grade_to_gpa(71) == 2.7
-    assert grade_to_gpa(70) == 2.7
+    return gpa
 
-    assert grade_to_gpa(69) == 0.0
-    assert grade_to_gpa(37) == 0.0
-    assert grade_to_gpa(0) == 0.0
-
-    with pytest.raises(ValueError):
-        grade_to_gpa(101)
-        grade_to_gpa(-1)
-
-
-def test_float_input():
-    """
-    Float inputs
-    """
-    with pytest.raises(TypeError):
-        grade_to_gpa(82.5)
-
-# add functions for any other tests
