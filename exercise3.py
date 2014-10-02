476145deaf9041b22ab74da61082fefb040683e3
goelalex <alexgoel@SPARE01-JAN29.2014>
2014-10-02 10:29:14 -0400

    Changed Dictionary
    Changed how dictionary was done

--- exercise3.py
+++ exercise3.py
@@ -4,6 +4,14 @@
 def decide_rps(player1, player2):
     return 1
 
-    rock_paper_scissors = {'Rock, Paper': 2, 'Rock, Scissors': 1,'Rock, Rock' : 0, 'Paper, Rock' : 1, 'Paper, Scissors': 2, 'Paper, Paper' : 0, 'Scissors, Rock' : 2, 'Scissors, Paper' : 1, 'Scissors, Scissors' :0 }
+    #rock_paper_scissors = {'Rock, Paper': 2, 'Rock, Scissors': 1,'Rock, Rock' : 0, 'Paper, Rock' : 1, 'Paper, Scissors': 2, 'Paper, Paper' : 0, 'Scissors, Rock' : 2, 'Scissors, Paper' : 1, 'Scissors, Scissors' :0 }
+    rps_results = {}
+    rps_results[("Rock", "Paper")] = 2
+    rps_results[("Paper", "Stone")] = 1
+    rps_results[("Paper", "Rock")] = 1
+    r
+
+    #prints result 1 which is Paper, Stone
+    print(rps_results[("Paper", "Stone")])
 
 
