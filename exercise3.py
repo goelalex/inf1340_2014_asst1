663f09f5b1b9ad8c17f6117e2256ccfa88b28cb5
goelalex <alexgoel@SPARE01-JAN29.2014>
2014-10-02 10:26:37 -0400

    Dictionary for RPS
    Re-did Dictionary for RPS

--- exercise3.py
+++ exercise3.py
@@ -4,5 +4,6 @@
 def decide_rps(player1, player2):
     return 1
 
-    rock_paper_scissors = {rock : paper, rock : scissors, rock : rock, paper : scissors, paper : paper, scissors : scissors}
+    rock_paper_scissors = {'Rock, Paper': 2, 'Rock, Scissors': 1,'Rock, Rock' : 0, 'Paper, Rock' : 1, 'Paper, Scissors': 2, 'Paper, Paper' : 0, 'Scissors, Rock' : 2, 'Scissors, Paper' : 1, 'Scissors, Scissors' :0 }
+
 
