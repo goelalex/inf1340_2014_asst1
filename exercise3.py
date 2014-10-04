7cd40768ff8203a0fd62acad66e91da96c3a008a
goelalex <alexgoel@SPARE01-JAN29.2014>
2014-10-04 09:31:42 -0400

    Added all 9 combos
    I added all 9 Rock, Paper, Scissors combinations and commented out some
    text which was causing an error.

--- exercise3.py
+++ exercise3.py
@@ -5,17 +5,21 @@ def decide_rps(player1, player2):
     return 1
 
     #rock_paper_scissors = {'Rock, Paper': 2, 'Rock, Scissors': 1,'Rock, Rock' : 0, 'Paper, Rock' : 1, 'Paper, Scissors': 2, 'Paper, Paper' : 0, 'Scissors, Rock' : 2, 'Scissors, Paper' : 1, 'Scissors, Scissors' :0 }
-<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
     rps_results = {("Paper", "Rock"): 1, ("Rock", "Paper"): 2, ("Paper","Paper"): 0, ("Paper", "Scissors",): 2, ("Scissors","Paper"): 1, ("Scissors", "Scissors"): 0, ("Rock", "Scissors"): 1, ("Scissors", "Rock"): 2, ("Rock", "Rock"): 0}
-    #rps_results[("Rock", "Paper")] = 2
-    #rps_results[("Paper", "Stone")] = 1
-    #rps_results[("Paper", "Rock")] = 1
-=======
+
+#=======
     rps_results = {}
     rps_results[("Rock", "Paper")] = 2
-    rps_results[("Paper", "Stone")] = 1
+    rps_results[("Rock", "Scissors")] = 1
+    rps_results[("Rock", "Rock")] = 0
+    rps_results[("Paper", "Scissors")] = 2
     rps_results[("Paper", "Rock")] = 1
->>>>>>> Stashed changes
+    rps_results[("Paper", "Paper")] = 0
+    rps_results[("Scissors", "Rock")] = 2
+    rps_results[("Scissors", "Paper")] = 1
+    rps_results[("Scissors", "Scissors")] = 0
+#>>>>>>> Stashed changes
 
 
     #prints result 1 which is Paper, Stone
